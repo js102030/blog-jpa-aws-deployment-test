f8f4e4059c3e81afa5d84f26008ea762ada5bc34
js <2kpppk@gmail.com>
2024-03-13 21:08:51 +0900

    parsedArticles 새로운 버전

--- src/main/java/com/estsoft/blogjpa/service/BlogService.java
+++ src/main/java/com/estsoft/blogjpa/service/BlogService.java
@@ -5,6 +5,7 @@ import com.estsoft.blogjpa.external.ExampleAPIClient;
 import com.estsoft.blogjpa.model.Article;
 import com.estsoft.blogjpa.repository.BlogRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -13,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Service
@@ -44,10 +46,8 @@ public class BlogService {
 
     @Transactional
     public Article update(Long id, AddArticleRequest request) {
-        // begin transaction
         Article article = findById(id);
         article.update(request.getTitle(), request.getContent());
-        // commit / rollback
         return article;
     }
 
@@ -58,12 +58,13 @@ public class BlogService {
         return article;
     }
 
+    @Transactional
     public List<Article> saveBulkArticles() {
         List<Article> articles = parsedArticles();
         return blogRepository.saveAll(articles);
     }
 
-    private List<Article> parsedArticles() {
+    private List<Article> parsedArticles_old() {
         String url = "https://jsonplaceholder.typicode.com/posts";
         String responseJson = apiClient.fetchDataFromApi(url);
 
@@ -79,5 +80,26 @@ public class BlogService {
         return jsonMapList.stream()
                 .map(hashMap -> new Article(hashMap.get("title"), hashMap.get("body")))
                 .toList();
+    }
+
+    private List<Article> parsedArticles() {
+        String url = "https://jsonplaceholder.typicode.com/posts";
+        String responseJson = apiClient.fetchDataFromApi(url);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<Article> articles = new ArrayList<>();
+
+        try {
+            List<Map<String, String>> jsonMapList = objectMapper.readValue(responseJson, new TypeReference<>() {
+            });
+
+            articles = jsonMapList.stream()
+                    .map(hashMap -> new Article(hashMap.get("title"), hashMap.get("body")))
+                    .toList();
+        } catch (JsonProcessingException e) {
+            log.error("Failed to parse json {}", e.getMessage());
+        }
+
+        return articles;
     }
 }
