f7146956875b79ff2f2ab56536f3685eeaaea094
js <2kpppk@gmail.com>
2024-03-17 20:38:52 +0900

    test code

--- src/test/java/com/estsoft/blogjpa/domain/comment/service/CommentServiceTest.java
+++ src/test/java/com/estsoft/blogjpa/domain/comment/service/CommentServiceTest.java
@@ -1,11 +1,20 @@
 package com.estsoft.blogjpa.domain.comment.service;
 
+import com.estsoft.blogjpa.domain.article.entity.Article;
+import com.estsoft.blogjpa.domain.article.service.ArticleService;
 import com.estsoft.blogjpa.domain.comment.dto.CommentPostRequest;
+import com.estsoft.blogjpa.domain.comment.dto.CommentResponse;
+import com.estsoft.blogjpa.domain.comment.dto.CommentUpdateRequest;
+import com.estsoft.blogjpa.domain.comment.entity.Comment;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
@@ -14,11 +23,64 @@ class CommentServiceTest {
 
     @Autowired
     CommentService commentService;
+    @Autowired
+    ArticleService articleService;
 
     @Test
     void create() {
         // given
-        CommentPostRequest request = new CommentPostRequest(1L, "content");
+        CommentPostRequest request = new CommentPostRequest(1L, "content505050");
 
+        // when
+        commentService.createComment(request);
+
+        // then
+        Article article = articleService.readWithComments(1L);
+        List<Comment> comments = article.getComments();
+        // comments를 돌면서 content가 일치하는 comment가 있는지 확인
+        assertThat(comments).extracting("content").contains("content505050");
+    }
+
+    @Test
+    void findById() {
+        // given
+        CommentPostRequest request = new CommentPostRequest(1L, "content505050");
+        CommentResponse comment = commentService.createComment(request);
+
+        // when
+        Comment foundComment = commentService.findById(comment.getId());
+
+        // then
+        assertThat(foundComment.getContent()).isEqualTo(comment.getContent());
+    }
+
+    @Test
+    void readComments() {
+        // given
+        CommentPostRequest request = new CommentPostRequest(1L, "content505050");
+        commentService.createComment(request);
+
+        // when
+        List<CommentResponse> comments = commentService.readComments();
+
+        // then
+        for (CommentResponse comment : comments) {
+            log.info("comment = {}", comment);
+        }
+        assertThat(comments).extracting("content").contains("content505050");
+    }
+
+    @Test
+    void updateComment() {
+        // given
+        CommentPostRequest request = new CommentPostRequest(1L, "content505050");
+        CommentResponse createResponse = commentService.createComment(request);
+
+        // when
+        CommentResponse commentResponse = commentService.updateComment(new CommentUpdateRequest(1L, "updatedContent"), createResponse.getId());
+        Comment comment = commentService.findById(commentResponse.getId());
+
+        // then
+        assertThat(comment.getContent()).isEqualTo("updatedContent");
     }
 }
